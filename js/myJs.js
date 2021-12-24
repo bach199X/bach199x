8e315e95da7fac81a43c4f10b829289ccaf93beb
bach199X <88819363+bach199X@users.noreply.github.com>
2021-12-24 22:13:50 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -36,7 +36,7 @@ $(document).ready(function () {
       title: textConfig.text1,
       text: textConfig.text2,
       imageUrl: "img/hiep.jpg",
-      imageWidth: 226,
+      imageWidth: 230,
       imageHeight: 380,
       background: '#fff url("img/iput-bg.jpg")',
       imageAlt: "Custom image",
